64812bbae1bfad774744a9c059da46f8e346f99e
maxim <maxim.mamonov@pr-solution.ru>
2018-08-14 07:22:59 +0300

    numbers to array

--- docker-exec.sh
+++ docker-exec.sh
@@ -3,8 +3,8 @@
 clear
 COUNT=1
 INDEX=1
-SUB_IND=0
 CONTAINER=$(docker ps --format "{{.Names}}")
+NUMBERS=("1", "2", "3", "4", "5", "6", "7", "8", "9")
 for item in $CONTAINER
 do
 	COUNT_ARR[$SUB_INT]=$item
@@ -14,7 +14,7 @@ done
 f1 () 
 {
 	echo "-----------"
-	echo "q - up / w - down / enter or 1-9 - exec to container / any key - exit /"
+	echo "q - up / w - down / enter or 1-9 - exec to container / any key - exit"
 	echo "-----------"
 
 	echo ""
@@ -53,12 +53,10 @@ wait ()
 		fi
 		
 	fi
-
-	if [[ $KEYCODE == "1" || $KEYCODE == "2" || $KEYCODE == "3" || $KEYCODE == "4" || $KEYCODE == "5" || $KEYCODE == "6" || $KEYCODE == "7" || $KEYCODE == "8" || $KEYCODE == "9" ]]
+	if [[ " ${NUMBERS[*]} " == *"$KEYCODE"* ]]
 	then
 		clear
 		docker exec -ti ${COUNT_ARR[$(($KEYCODE - 1))]} bash
-		echo ${COUNT_ARR[$(($KEYCODE - 1))]}
 		exit
 	fi
 	if [[ $KEYCODE == "" ]]
